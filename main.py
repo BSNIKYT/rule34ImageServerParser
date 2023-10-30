f63b5ee765080dea9ef612d396e9cc340e6ece22
→«★BS NIK★»← <74881427+BSNIKYT@users.noreply.github.com>
2023-10-31 02:56:36 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
+
+import datetime
 from modules.colors import *
 from modules.functions import *
 from modules.downloader import *
 from modules.statisticksDEF import Statistics
 from modules.statisticksDEF import GlobalTagsInfo
+from modules.statisticksDEF import Guard
+
 import json
 import time 
 
 format_link = 'https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags=cat_ears'
-api_link = 'https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags='
+api_link = 'https://api.rule34.xxx/index.php?page=dapi&s=post&q=index&tags=' 
 
 format_enter_text = f'''
-Format: {format_link}
+Format: {format_link} 
 '''
+working_dir = os.getcwd()
+if str(os.name) == "nt":dir_pref = "\\"
+else:dir_pref = "/"
 
 
-GuardOnline = False # DEV 
+
+try:
+  os.mkdir("logging")
+  os.chdir(f'{working_dir + dir_pref + "logging"}')
+  py_logger, starting_script_time = set_logger_settings()
+except FileExistsError:
+  os.chdir(f'{working_dir + dir_pref + "logging"}')
+  py_logger, starting_script_time = set_logger_settings()
+  pass
+except PermissionError:
+  py_logger, starting_script_time = set_logger_settings()
+  pass
+
 
 
 def inputUrl():
@@ -29,14 +48,27 @@ def inputUrl():
     check_internet()
     return tags
 
+
+
 def main():
+    S = Statistics(working_dir=os.getcwd(), logger=py_logger)
+    T = GlobalTagsInfo(py_logger)
+    G = Guard(py_logger)
 
     urls = []
     tags = ''
 
     input_tags = inputUrl()
+
+
+
+    t = {f"{str(datetime.now()).split('.')[0]}": input_tags}
+    S.statistics_format_function_ok(t)
+    py_logger.info(f'[MAIN] User entered tags: {input_tags}')
     tags = formating_tags_list(input_tags, ind= 1)
-    if tags == '':return f'{red}[ERROR] {red}You have not entered tag(s): _{tags}_ ...{white}'
+    if tags == '':
+        py_logger.error(f'[MAIN] The user did not enter any tags.: {input_tags}')
+        return f'{red}[ERROR] {red}You have not entered tag(s): _{tags}_ ...{white}'
 
 
 
@@ -44,10 +76,15 @@ def main():
     count, offset = find_count(f'{api_link}{tags}&pid=0')
     print(f'''Count:{count}
 {'-'*15}''')
+    
+    py_logger.info(f'''{'='*10}- [PARSER] -{'='*10}''')
+    py_logger.info(f'[PARSER] Count: {count}')
 
+    py_logger.info(f'''{'-'*8}- [Links] -{'-'*8}''')
     for i in range(0, round(count/100)+1):
         link = f'{api_link}{tags}&pid={i}'
-
+        py_logger.info(f'[PARSER] [LINK][{i}]: {link}')
+        
         urls.append(link)
     
     
@@ -55,9 +92,8 @@ def main():
     download_links = []
     GlobalTags = []
     
-    S = Statistics(os.getcwd())
-    T = GlobalTagsInfo()
-    s = [S,T]
+    
+    s = [S,T,G]
 
     for link in urls:
             print('\r', end='')
@@ -74,20 +110,29 @@ def main():
             
     
     download_links = list(set(download_links))
-    with open('data.json', 'w') as file:json.dump(data, file, indent=4)
-    with open('download_links.json', 'w') as file:json.dump(download_links, file, indent=4)
+
+    name_folder = nameFolder(input_tags)
+    if not os.path.exists(name_folder):
+        os.mkdir(name_folder)
+    wdir = os.getcwd()
+    os.chdir(name_folder)
+
+    print(f'''\n{'='*10}- STATS -{'='*10}\n''')
+
     S.writeStats()
     T.ch_new_tags(input_tags = GlobalTags)
     T.writeTags()
-    print(f'''\n{'='*10}- Download -{'='*10}\n''')
 
-    if input('Скачать? (Y/N) >>> ') == 'Y':
-        if len(input_tags.split(' ')) == 1:
-            nf = str(input_tags.split(' ')[0])
-        else:
-            nf = 'pictires'
-        print(nf)
-        download(download_links, nf)
+
+    os.chdir(wdir)
+    print(f'''\n{'='*10}- Download -{'='*10}''')
+
+    if len(download_links) != 0:
+        if input('\nDownload? (Y/N) >>> ') == 'Y':
+            os.chdir(name_folder)
+            download(download_links)
+    else:
+       print(f'{red}[!] There is nothing to download!{white}')
 
     
 

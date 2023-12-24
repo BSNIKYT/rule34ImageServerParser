6c1344452e16783397ccb2b8190978adac6e7226
→«★BS NIK★»← <74881427+BSNIKYT@users.noreply.github.com>
2023-12-24 16:52:47 +0300

    Update statisticksDEF.py

--- modules/statisticksDEF.py
+++ modules/statisticksDEF.py
@@ -21,8 +21,23 @@ else:
 
 
 class Config():
-    def __init__(self, logger):
+    """
+    Класс для управления конфигурацией.
 
+    Attributes:
+        logger: Объект логгера для записи сообщений.
+        remote: Флаг, указывающий на использование удаленного режима.
+        GuardTagsInverse: Флаг инвертирования блокированных тегов для Guard.
+        GuardOnline: Флаг активации Guard.
+        BlockedTags: Список заблокированных тегов для Guard.
+    """
+    def __init__(self, logger):
+        """
+        Инициализирует объект конфигурации, загружая данные из файла `config.json`.
+
+        Args:
+            logger: Объект логгера для записи сообщений.
+        """
         self.logger = logger
         self.logger.info('[Config] module has beel successfully loaded.')
         # reload_file_config(os.getcwd())[0]
@@ -41,6 +56,16 @@ class Config():
         
 
     def reload_config(self, path, filename='config.json'):
+        """
+        Перезагружает конфигурацию из файла.
+
+        Args:
+            path: Путь к директории, где расположен файл конфигурации.
+            filename (str, optional): Имя файла конфигурации. По умолчанию 'config.json'.
+
+        Returns:
+            dict: Словарь с данными конфигурации.
+        """
         statictics_file_name = filename
         if not os.path.exists(path + dir_pref + statictics_file_name):
             q = []
@@ -80,7 +105,23 @@ class Config():
 
 
 class Guard():
+    """
+    Класс для реализации Guard - фильтрации изображений по тегам.
+
+    Attributes:
+        logger: Объект логгера для записи сообщений.
+        online_val: Флаг активации Guard.
+        BlockedTags: Список заблокированных тегов для Guard.
+        GuardTagsInverse: Флаг инвертирования блокированных тегов для Guard.
+        guardON: Флаг состояния Guard.
+    """
     def __init__(self, logger):
+        """
+        Инициализирует объект "Guard", загружая настройки из файла конфигурации.
+
+        Args:
+            logger: Объект логгера для записи сообщений.
+        """
         self.logger = logger
         self.logger.info('[Guard] module has beel successfully loaded.')
         self.config_data = reload_config(os.getcwd())[0]
@@ -108,6 +149,17 @@ class Guard():
             self.guardON = False
 
     def GuardCheck(self, tags, file_url, _id):
+        """
+        Проверяет теги изображения на соответствие списку заблокированных тегов.
+
+        Args:
+            tags: Список тегов изображения.
+            file_url: URL файла изображения.
+            _id: Идентификатор изображения.
+
+        Returns:
+            bool: True, если изображение прошло проверку Guard, False в противном случае.
+        """
         if self.config_data['GuardOnline']:
             if isinstance(tags, list):
                 for tag in tags:
@@ -139,12 +191,31 @@ class Guard():
 
 
 class GlobalTagsInfo():
+    """
+    Класс для отслеживания глобальных тегов.
+
+    Attributes:
+        logger: Объект логгера для записи сообщений.
+        tagsGlobal: Список глобальных тегов.
+    """
     tagsGlobal = []
 
     def __init__(self, logger):
+        """
+        Инициализирует объект для отслеживания глобальных тегов.
+
+        Args:
+            logger: Объект логгера для записи сообщений.
+        """
         self.logger = logger
 
     def ch_new_tags(self, input_tags):
+        """
+        Обновляет список глобальных тегов.
+
+        Args:
+            input_tags: Новые теги, которые нужно добавить.
+        """
         if not isinstance(input_tags, list):
             input_tags = input_tags.split(' ')
         for new_or_old_tags in input_tags:
@@ -152,6 +223,15 @@ class GlobalTagsInfo():
                 self.tagsGlobal.append(input_tags)
 
     def FrSTRtoList(self, input_tags):
+        """
+        Преобразует строку тегов в список.
+
+        Args:
+            input_tags: Строка тегов.
+
+        Returns:
+            list: Список тегов.
+        """
         tagsOut = []
         self.data_load = self.input_tags
         for i in range(0, len(self.data_load)):
@@ -165,6 +245,9 @@ class GlobalTagsInfo():
         return tagsOut
 
     def writeTags(self):
+        """
+        Записывает текущий список глобальных тегов в файл "GlobalTagsInput.json".
+        """
         data_load = self.tagsGlobal
         tagsOut = []
         for i in range(0, len(data_load)):
@@ -185,8 +268,23 @@ class GlobalTagsInfo():
 
 
 class Statistics():
+    """
+    Класс для отслеживания статистики сессии.
+
+    Attributes:
+        logger: Объект логгера для записи сообщений.
+        working_directory: Рабочая директория.
+        data_stats: Статистика сессии.
+    """
 
     def __init__(self, working_dir, logger):
+        """
+        Инициализирует объект для отслеживания статистики сессии.
+
+        Args:
+            working_dir: Рабочая директория.
+            logger: Объект логгера для записи сообщений.
+        """
         self.logger = logger
         self.logger.info('[Statistics] module has beel successfully loaded.')
         self.logger.info(f"[Statistics] {'='*11}")
@@ -198,12 +296,27 @@ class Statistics():
         self.data_stats = self.data_load
 
     def statistics_format_function_ok(self, data):
+        """
+        Добавляет успешное событие в статистику.
+
+        Args:
+            data: Данные о событии.
+        """
         self.data_stats.append(data)
 
     def statistics_format_function_error(self, data):
+        """
+        Добавляет ошибочное событие в статистику.
+
+        Args:
+            data: Данные о событии.
+        """
         self.data_stats.append(data)
 
     def writeStats(self):
+        """
+        Записывает статистику в файл "statistics.json".
+        """
         with open("statistics.json", "w") as outfile:
             json.dump(self.data_stats, outfile, indent=4)
         self.logger.info('[Statistics] File created [statistics.json]')
@@ -211,11 +324,20 @@ class Statistics():
 
 
 class CheckAvailableMemoryDevice():
+    """
+    Класс для проверки доступной памяти устройства.
+    """
 
     def __init__(self):
+        """
+        Инициализирует объект для проверки доступной памяти устройства.
+        """
         pass
 
     def check(self):
+        """
+        Пустой метод. Находится в бесконечном цикле, предположительно для постоянной проверки доступной памяти (не реализовано).
+        """
         dostypnaia_pamuat = True
         while dostypnaia_pamuat:
             pass
